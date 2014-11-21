7dd336f4ba64f840f46607021999b94ef5f1337c
ClementGendry <clement.gendry@gmail.com>
2014-11-21 16:51:43 +0100

    New changes on our interface

--- js/script.js
+++ js/script.js
@@ -1,58 +1,74 @@
-$(document).ready(function(){
+$('document').ready(function () {
+    
+Dropzone.options.dropzone = { // The camelized version of the ID of the form element
+    // The configuration we've talked about above
+    url: '#',
+    previewsContainer: ".dropzone-previews",
+    uploadMultiple: true,
+    parallelUploads: 100,
+    maxFiles: 100
+  }
 
-    $('#sign_in').click(function(){
+    $('#sign_in').click(function () {
         $('#not_logged').animate({
             right: '-50%',
             display: 'none'
-        }, 'slow' );
+        }, 'slow');
 
         $('#logged_in').css({
-          'display': 'block',
-          'right': '-35%'
+            display: 'block',
+            right: '-35%'
         });
 
         $('#sign_form').animate({
             right: "0%"
-        }, 'slow' );
+        }, 'slow');
 
     });
 
-    $('#sign_form > button').click(function(){
+    $('#sign_form > button').click(function () {
         $('.logs').animate({
             right: '0%'
-        }, 'slow' );
+        }, 'slow');
 
         $('#sign_form').animate({
             right: '-35%'
-        }, 'slow' );
+        }, 'slow');
 
         $('#infos').animate({
             top: '110%'
-        }, 'slow' );
+        }, 'slow');
 
         $('#send_form').animate({
             top: '18%'
-        }, 'slow' );
+        }, 'slow');
     });
 
-    $('#choice_tab').mouseover(function(){
+    $('#choice_tab').click(function () {
         $('ul').slideDown('slow');
+        if ($('ul').is(':visible')) {
+            $('#choice_tab').click(function () {
+                $('ul').slideUp('slow');
+            });
+        } else {
+            $('ul').css('display', 'block');
+        }
     });
-
-    $('ul:last-child').click(function(){
+    
+    $('ul:last-child').click(function () {
         $('ul').slideUp('slow');
 
-        $('#logged_in').delay( 600 ).animate({
-            right: '-50%',
-        }, 'slow' );
+        $('#logged_in').delay(600).animate({
+            right: '-50%'
+        }, 'slow');
 
         $('#infos').animate({
             top: '18%'
-        }, 'slow' );
+        }, 'slow');
 
         $('#send_form').animate({
             top: '-110%'
-        }, 'slow' );
+        }, 'slow');
     });
 
 });
